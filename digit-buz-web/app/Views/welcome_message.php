458d50babb95dfa6a87db3ad7c9b351c87b82e7a
Dinuka Bandara <dinuka@smartlife-labs.com>
2023-07-05 17:28:48 +0530

    navbar responsive

--- digit-buz-web/app/Views/welcome_message.php
+++ digit-buz-web/app/Views/welcome_message.php
@@ -13,18 +13,22 @@
 
 <body class="bg-color0">
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light p-0">
-    <a class="navbar-brand p-3 pl-4" href="#">
+  <nav class="navbar navbar-expand-lg fixed-top bg-light p-0">
+    <div class="navbar-header d-flex">
+      <div id="nav-icon1" class="navbar-toggle ml-4 d-lg-none collapsed" data-toggle="collapse"
+        data-target="#navbarNavDropdown" aria-expanded="false" aria-controls="navbarNavDropdown">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <a class="navbar-brand p-3 pl-4" href="#">
+        <span class="logo-text-left">DIGIT</span>
+        <span class="logo-text-right">BUZ</span>
+      </a>
 
-      <span class="logo-text-left">DIGIT</span>
-      <span class="logo-text-right">BUZ</span>
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-      aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav ml-auto mr-5">
+    </div>
+    <div class="navbar-collapse collapse" id="navbarNavDropdown">
+      <ul class="nav navbar-nav">
         <li class="nav-item active">
           <a class="nav-link" href="#">
 
@@ -47,23 +51,56 @@
             <a class="dropdown-item" href="#">Something else here</a>
           </div>
         </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="#">
+      </ul>
 
-            <?php echo $t1 ?>
+      <ul class="nav navbar-nav navbar-right-buttons">
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Option1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Option2</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Account
           </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink2">
+            <a class="dropdown-item" href="#">Action1</a>
+            <a class="dropdown-item" href="#">Another action1</a>
+            <a class="dropdown-item" href="#">Something else here1</a>
+          </div>
         </li>
       </ul>
+
     </div>
   </nav>
   <div class="body-content">
-    <!-- <?php
-    // include('widgets/trading_view_widget1.php');
-    ?> -->
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <div class="footer">
-    <img src="./logo.png" width="238" height="60" class="d-inline-block align-top" alt="">
-  </div>
+  <?=
+    $this->include('layout/footer');
+  ?>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
     integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
     crossorigin="anonymous"></script>
@@ -73,6 +110,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
     integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
     crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/a13e0df62b.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
