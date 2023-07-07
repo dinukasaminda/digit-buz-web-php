1d4e4a24626f8ce7d23c2d20c1a583b67e801115
Dinuka Bandara <dinuka@smartlife-labs.com>
2023-07-07 23:01:33 +0530

    add title

--- digit-buz-web/app/Views/welcome_message.php
+++ digit-buz-web/app/Views/welcome_message.php
@@ -89,7 +89,7 @@
       <div class="col-sm-6">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Special title treatment</h5>
+            <h5 class="card-title">World of crypto</h5>
             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
